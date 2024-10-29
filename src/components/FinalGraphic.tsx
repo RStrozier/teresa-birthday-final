00a515c500039d8169ac8755a9ebe2def36e29ed
Rashida Strozier <rashidastrozier@Rashidas-Air.lan>
2024-10-29 06:29:28 -0400

    did something

--- src/components/FinalGraphic.tsx
+++ src/components/FinalGraphic.tsx
@@ -1,16 +1,17 @@
 const FinalGraphic = () => {
-    return (
-      <>
+  return (
+    <>
       <div style={{ position: 'relative', padding: 0, margin: 0, height: '100%', backgroundColor: 'black', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
         <iframe
-          src="https://player.vimeo.com/video/1024235049?badge=0&autopause=0&controls=0&autoplay=1&loop=1&muted=1&title=0&byline=0&portrait=0&app_id=58479"
+          src="https://player.vimeo.com/video/1024303963?badge=0&autopause=0&controls=0&autoplay=1&loop=1&muted=1&title=0&byline=0&portrait=0" // Updated link with parameters
           allow="autoplay; fullscreen;"
           style={{ width: '100%', maxWidth: '800px', height: 'auto', aspectRatio: '16/9', border: 'none' }}
+          title="Teresa-Wonderfully-Made"
         ></iframe>
       </div>
       <div className="spacer"></div>
-      </>
-    );
-  }
-  
-  export default FinalGraphic;
+    </>
+  );
+}
+
+export default FinalGraphic;
