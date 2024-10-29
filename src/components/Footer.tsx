a03c06b3f5ec793e3474c2cbf739588a9fa1fd5f
Rashida Strozier <rashidastrozier@Rashidas-Air.lan>
2024-10-29 06:38:58 -0400

    knaxed site in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
      <div className="footer">
         <div className="spacer"></div>
-        <div className="poppins-light">Site created for Teresa, designed by Rashida 
+        <div className="poppins-light">Created for Teresa, designed by Rashida 
             including
             all content & code, 
             <br />
