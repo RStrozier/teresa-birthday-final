72d389a3a01a279be8abf1fa16075141383f70a7
Rashida Strozier <rashidastrozier@Rashidas-Air.lan>
2024-10-29 06:20:31 -0400

    added big son to the video

--- src/components/BirthdayVideo.tsx
+++ src/components/BirthdayVideo.tsx
@@ -2,12 +2,12 @@ const BirthdayVideo = () => {
   return (
     <div style={{ position: 'relative', paddingTop: '56.25%' }}>
       <iframe 
-        src="https://player.vimeo.com/video/1024263723?badge=0&autopause=0&player_id=0&app_id=58479&title=0&byline=0&portrait=0" 
+        src="https://player.vimeo.com/video/1024300527?badge=0&autopause=0&player_id=0&app_id=58479&title=0&byline=0&portrait=0" 
         allow="autoplay; fullscreen; picture-in-picture; clipboard-write" 
         style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }} 
         frameBorder="0" 
         allowFullScreen 
-        title="Teresa-final-site-edit"
+        title="Final-Teresa-Birthday-Movie"
       ></iframe>
     </div>
   );
