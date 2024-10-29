26fa0bd8cad7a6fa387570f3b48632dd1a47a194
Rashida Strozier <rashidastrozier@Rashidas-Air.lan>
2024-10-29 05:21:22 -0400

    added the cpt ocmment

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,14 +4,17 @@ const Footer = () => {
     return (
      <div className="footer">
         <div className="spacer"></div>
-        <div className="poppins-light">Site created for Teresa, designed by Rashida including
-            video content, 
+        <div className="poppins-light">Site created for Teresa, designed by Rashida 
+            including
+            all content & code, 
             and executed by Pastor Dave. 
             <br/>
             Thank you to all else who contributed,
             including Teresa's children, brother, family members, and friends.
             <div className="mini-spacer"></div>
             <div className="bottom-text poppins-medium">P.S. Pastor Dave singing was fire!</div>
+            <div className="poppins-light sm-text"><i>Please excuse any typo's, all content 
+                was made on CPT</i></div>
         </div>
      </div>
     )
