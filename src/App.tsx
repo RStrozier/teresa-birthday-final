e54d871d9275d4346172c152d290338e3d4e4aca
Rashida Strozier <rashidastrozier@Rashidas-Air.lan>
2024-10-29 00:00:46 -0400

    second push

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import BirthdayVideo from './components/BirthdayVideo'
+
+function App() {
+
+
+  return (
+    <>
+     <BirthdayVideo />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/BirthdayVideo.tsx
@@ -0,0 +1,16 @@
+
+const BirthdayVideo = () => {
+    return (
+      <div style={{ position: 'relative', paddingTop: '56.25%' }}>
+        <iframe 
+          src="https://player.vimeo.com/video/1024215575?badge=0&autopause=0&controls=1&autoplay=1&loop=1&title=0&byline=0&portrait=0&app_id=58479" 
+          allow="autoplay; fullscreen; picture-in-picture; clipboard-write" 
+          style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }} 
+          frameBorder="0" 
+          allowFullScreen 
+        ></iframe>
+      </div>
+    );
+  }
+  
+  export default BirthdayVideo
