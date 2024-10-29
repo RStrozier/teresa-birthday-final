821dd975166f871dd98feaf9c57f765105c23dfc
Rashida Strozier <rashidastrozier@Rashidas-Air.lan>
2024-10-29 06:36:52 -0400

    added co workers for future video

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,10 +11,10 @@ const Footer = () => {
             and <span className="purple-text">executed</span> by Pastor Dave. 
             <br/>
             Thank you to all else who contributed,
-            including Teresa's children, brother, family members, and friends.
+            including Teresa's children, brother, family members, co-workers, and friends.
             <div className="mini-spacer"></div>
             <div className="bottom-text poppins-medium">P.S. Pastor Dave singing was fire!</div>
-            <div className="poppins-light sm-text"><i>Please excuse any typo's, all content 
+            <div className="poppins-light sm-text"><i>All content 
                 was made on CPT</i></div>
         </div>
      </div>
