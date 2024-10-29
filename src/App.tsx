97a9397f94849b8f38bd8ea26a6d7b8bef9e8843
Rashida Strozier <rashidastrozier@Rashidas-Air.lan>
2024-10-29 03:12:26 -0400

    believe close to final push, pushed footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css'
 import BirthdayMessage from './components/BirthdayMessage'
 import BirthdayVideo from './components/BirthdayVideo'
 import FinalGraphic from './components/FinalGraphic'
+import Footer from './components/footer'
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
      <BirthdayVideo />
      <BirthdayMessage /> 
      <FinalGraphic />
+     <Footer />
     </>
   )
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,17 @@
+
+
+const Footer = () => {
+    return (
+     <div className="footer">
+        <div className="spacer"></div>
+        <div className="poppins-medium">Site created for Teresa, designed by Rashida 
+            and executed by Pastor Dave. Thank you to all else who contributed,
+            including Teresa's children, brother, family members, and friends.
+            <div className="mini-spacer"></div>
+            <div className="bottom-text poppins-medium">P.S. Pastor Dave singing was fire! That was crazy right?!</div>
+        </div>
+     </div>
+    )
+  }
+  
+  export default Footer
